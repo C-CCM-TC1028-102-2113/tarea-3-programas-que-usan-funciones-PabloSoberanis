fa6bebf9292579de263f115bdfcf86072ed7c591
PabloSoberanis <89001142+PabloSoberanis@users.noreply.github.com>
2021-08-30 22:51:08 -0500

    Update exercise.py

--- assignments/10Bisiesto/src/exercise.py
+++ assignments/10Bisiesto/src/exercise.py
@@ -1,7 +1,18 @@
+def es_bisiesto(a):
+    if a % 4 != 0: 
+	    return False
+    elif a % 4 == 0 and a % 100 != 0:
+	    return True
+    elif a % 4 == 0 and a % 100 == 0 and a % 400 != 0: 
+	    return False
+    elif a % 4 == 0 and a % 100 == 0 and a % 400 == 0: 
+	     return True
 
 def main():
-    #escribe tu código abajo de esta línea
-    pass
+  
+  añoU = int(input())#el año que queremos comprobar
+  res = es_bisiesto(añoU)
+  print(str(res))
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
