92e7d080aa03aec326e3ae456d946d6ad09f7e23
PabloSoberanis <89001142+PabloSoberanis@users.noreply.github.com>
2021-08-30 21:21:38 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -1,6 +1,11 @@
 
 def main():
     #escribe tu código abajo de esta línea
+    x = float(input("Dame la base: "))
+    y = float(input("Dame la Altura: "))
+    z = float(input("Dame la profunidad: "))
+    volumen = x*y*z
+    print(volumen)
     pass
 
 if __name__=='__main__':
